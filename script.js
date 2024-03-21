4061d7ff86fb4b4947d5b0c541855364a0eab631
Sammantha <slyvranken@gmail.com>
2024-03-20 21:53:36 -0700

    Add user prompt for grid size

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const container = document.querySelector(".container");
 let row;
 let column;
+let gridSize = 16;
 
 function createGrid(){
-    for(row = 0; row < 16; row++){
+    for(row = 0; row < gridSize; row++){
         const rowDiv = document.createElement("div");
         container.appendChild(rowDiv);
         rowDiv.classList.add("row");
-        for(column = 0; column < 16; column++){
+        for(column = 0; column < gridSize; column++){
             const columnDiv = document.createElement("div");
             rowDiv.appendChild(columnDiv);
             columnDiv.classList.add("square");
             console.log("Made a square!");
-
+            columnDiv.addEventListener("mousemove", () => {
+                columnDiv.classList.toggle("colored");
+            })
         }
     }
 }
 
 createGrid();
+
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    gridSize = prompt("Enter the grid size: ");
+    createGrid();    
+    }
+);
